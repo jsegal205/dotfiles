859baa9e61a028aaedddc47e01a62b444755f53c
Jim Segal <hello@jimsegal.com>
2019-11-07 19:56:10 -0600

    add dot files and a fresh setup script

---
+++ .zshrc
@@ -0,0 +1,37 @@
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="robbyrussell"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+plugins=(
+  aws
+  brew
+  bundler
+  gem
+  git
+  npm
+  pip
+  rails
+  redis-cli
+  ruby
+  vscode
+  zsh-syntax-highlighting
+)
+
+# User configuration
+export PATH="$HOME/.node/bin:$PATH"
+
+source $ZSH/oh-my-zsh.sh
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+export PATH="/usr/local/bin:$PATH"
+
+. /usr/local/opt/asdf/asdf.sh
---
+++ fresh-setup.sh
@@ -0,0 +1,88 @@
+# !/bin/bash
+
+# will likely need permissions to run
+# cd path/to/file
+# chmod +x fresh-setup.sh
+
+echo "=== Installing xcode cli ==="
+xcode-select --install
+
+if test ! $(which brew); then
+  echo "=== Installing homebrew ==="
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+
+brew cleanup
+
+brew install zsh
+
+echo "=== Installing Oh My Zsh ==="
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+PACKAGES=(
+  asdf
+  coreutils
+  git
+  gpg
+  mysql
+  npm
+  openssl
+  yarn
+)
+
+echo "=== Installing brew packages ==="
+brew install ${PACKAGES[@]}
+
+brew cleanup
+
+CASKS=(
+  firefox
+  google-chrome
+  iterm2
+  slack
+  visual-studio-code
+)
+
+echo "=== Installing brew casks ==="
+brew cask install ${CASKS[@]}
+
+ASDF_PLUGINS=(
+  "nodejs https://github.com/asdf-vm/asdf-nodejs.git"
+  "ruby https://github.com/asdf-vm/asdf-ruby.git"
+)
+
+echo "=== Installing asdf plugins ==="
+for asdf_plugin in "${ASDF_PLUGINS[@]}"; do
+  asdf plugin-add $asdf_plugin
+done
+
+# need to update the keyring per nodejs asdf plugin
+# https://github.com/asdf-vm/asdf-nodejs#install
+echo "=== Setting keyring for asdf NodeJS ==="
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+ASDF_VERSIONS=(
+  "nodejs 12.12.0"
+  "ruby 2.6.3"
+)
+
+echo "=== Installing asdf versions ==="
+for asdf_version in "${ASDF_VERSIONS[@]}"; do
+  asdf install $asdf_version
+  asdf global $asdf_version
+done
+
+echo "=== Copying rc files ==="
+RC_FILES=(
+  .gemrc
+  .vimrc
+  .zshrc
+)
+for file in "${RC_FILES[@]}"; do
+  cp $file ~/$file
+done
+
+echo "=== Remember to run the following command after all is said and done ==="
+echo "source ~/.zshrc"
