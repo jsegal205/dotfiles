d2db8d73ede58bbbcd3db81abf8dfb74f9674664
Jim Segal <hello@jimsegal.com>
2023-01-08 12:18:32 -0600

    update brew install command

--- fresh-setup.sh
+++ fresh-setup.sh
@@ -49,7 +49,7 @@ killall Dock
 
 if test ! $(which brew); then
   echo "=== Installing homebrew ==="
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jimsegal/.zprofile
 fi
 
