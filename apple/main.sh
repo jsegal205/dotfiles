b7bc297b1883dc54dd9c1a34a0199f03d94aeed6
Jim Segal <hello@jimsegal.com>
2023-11-25 10:57:41 -0600

    make script idempotent

--- apple/main.sh
+++ apple/main.sh
@@ -63,7 +63,16 @@ log "Remove default programs that are bundled with OSX"
 sudo rm -rf /Applications/iMovie.app
 sudo rm -rf /Applications/GarageBand.app
 
-log "Creating default folders I always use"
-mkdir -p ~/personal
-mkdir -p ~/work
-mkdir -p ~/Desktop/slack-downloads
+DEFAULT_DIRS=(
+  ~/Desktop/slack-downloads
+  ~/projects
+  ~/personal
+  ~/work
+)
+
+for dir in "${DEFAULT_DIRS[@]}"; do
+  if [ ! -d "$dir" ]; then
+    log "Creating $dir"
+    mkdir -p $dir
+  fi
+done
